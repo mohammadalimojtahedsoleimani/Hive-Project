4f5fda76e71769061521766ab8dfadc4e79ec760
amir-reza-programmer <amirrezazare130081@gmail.com>
2023-05-17 21:21:25 +0330

    update config

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
-import React , { useEffect } from 'react';
-
 import { Route , Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import PostsPage from "./pages/PostsPage";
-import { LoginModalProvider } from "./context/LoginContext";
-import { ForgetModalProvider } from "./context/forgetPassContext";
-import CharityProvider from "./context/CharityProvider"
-import { SigunUpProvider } from "./context/SignUpContext"
-import { DakhelProvider } from "./context/DakhelContext";
-import { SearchProvider } from "./context/SearchContext";
-import ProfilePage from './pages/ProfilePage';
-import PostPage from './pages/PostPage';
-import NotFoundPage from './pages/NotFoundPage';
-import EmailConfigurationPage from './pages/EmailConfigurationPage';
-import ChangePasswordPage from './pages/ChangePasswordPage';
-import SignUpPage from "./pages/SignUpPage";
+import LandingPage from './mainPages/LandingPage'
+import PostsPage from "./mainPages/PostsPage";
+import PostPage from "./mainPages/PostPage";
+import DashboardPage from "./mainPages/DashboardPage";
 
-function App () {
-    useEffect ( () => {
-        document.title = "Hive";
+function App() {
 
-    } , [] )
-    return (
-        <CharityProvider>
-
-
-            <div>
-                <SigunUpProvider>
-                    <DakhelProvider>
-
-
-                        <LoginModalProvider>
-                            <ForgetModalProvider>
-                                <SearchProvider>
-
-
-                                    <Routes>
-                                        <Route path="/" element={ <LandingPage/> }/>
-                                        <Route path="/page/:id" element={ <PostPage/> }/>
-                                        <Route path="/profile" element={ <ProfilePage/> }/>
-                                        <Route path="/Posts" element={ <PostsPage/> }/>
-                                        <Route path="/SignUp" element={ <SignUpPage/> }/>
-                                        <Route path="/activation/*" element={ <EmailConfigurationPage/> }/>
-                                        <Route path="/reset_password/*" element={ <ChangePasswordPage/> }/>
-                                        <Route path="/*" element={ <NotFoundPage/> }/>
-                                    </Routes>
-                                </SearchProvider>
-                            </ForgetModalProvider>
-                        </LoginModalProvider>
-                    </DakhelProvider>
-                </SigunUpProvider>
-            </div>
-        </CharityProvider>
-    );
+  return (
+    <main>
+        <Routes>
+            <Route path="/" element={ <LandingPage/> }/>
+            <Route path="/Posts" element={ <PostsPage/> }/>
+            <Route path="/Post" element={ <PostPage/> }/>
+            <Route path="/dashboard" element={ <DashboardPage/> }/>
+            {/* <Route path="/page/:id" element={ <PostPage/> }/>
+            <Route path="/profile" element={ <ProfilePage/> }/>
+            <Route path="/Posts" element={ <PostsPage/> }/>
+            <Route path="/activation/*" element={ <EmailConfigurationPage/> }/>
+            <Route path="/reset_password/*" element={ <ChangePasswordPage/> }/>
+            <Route path="/*" element={ <NotFoundPage/> }/> */}
+        </Routes>
+    </main>
+  )
 }
 
-export default App;
+export default App
