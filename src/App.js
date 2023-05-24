d95a0f2bf45f9c45a49517b18fa65b65056d9a59
agha mamali <soleimanyali88@gmail.com>
2023-05-24 16:12:39 +0330

    confirmed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
 import { Route , Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import PostsPage from "./pages/PostsPage";
-import { LoginModalProvider } from "./context/LoginContext";
-import { ForgetModalProvider } from "./context/forgetPassContext";
-import CharityProvider from "./context/CharityProvider"
-import { SigunUpProvider } from "./context/SignUpContext"
-import { DakhelProvider } from "./context/DakhelContext";
-import { SearchProvider } from "./context/SearchContext";
-import ProfilePage from './pages/ProfilePage';
-import PostPage from './pages/PostPage';
-import NotFoundPage from './pages/NotFoundPage';
-import EmailConfigurationPage from './pages/EmailConfigurationPage';
-import ChangePasswordPage from './pages/ChangePasswordPage';
-import SignUpPage from "./pages/SignUpPage";
+import LandingPage from './mainPages/LandingPage'
+import PostsPage from './mainPages/PostsPage';
+import DashboardPage from './mainPages/DashboardPage';
+import PostPage from './mainPages/PostPage'
+import NotFoundPage from "./components/mainPages/NotFound/NotFoundPage";
 
-function App() {
 
-    } , [] )
+function App () {
+
     return (
-        <CharityProvider>
 
+            <Routes>
+                <Route path="/" element={ <LandingPage/> }/>
+                <Route path="/posts" element={ <PostsPage/> }/>
+                <Route path="/post" element={ <PostPage/> }/>
+                <Route path="/dashboard" element={ <DashboardPage/> }/>
+                <Route path='/*' element={ <NotFoundPage/>}/>
 
-            <div>
-                <SigunUpProvider>
-                    <DakhelProvider>
+                {/* <Route path="/page/:id" element={ <PostPage/> }/>
+            <Route path="/profile" element={ <ProfilePage/> }/>
+            <Route path="/Posts" element={ <PostsPage/> }/>
+            <Route path="/activation/*" element={ <EmailConfigurationPage/> }/>
+            <Route path="/reset_password/*" element={ <ChangePasswordPage/> }/>
+            <Route path="/*" element={ <NotFoundPage/> }/> */ }
+                    </Routes>
 
-
-                        <LoginModalProvider>
-                            <ForgetModalProvider>
-                                <SearchProvider>
-
-
-                                    <Routes>
-                                        <Route path="/" element={ <LandingPage/> }/>
-                                        <Route path="/page/:id" element={ <PostPage/> }/>
-                                        <Route path="/profile" element={ <ProfilePage/> }/>
-                                        <Route path="/Posts" element={ <PostsPage/> }/>
-                                        <Route path="/SignUp" element={ <SignUpPage/> }/>
-                                        <Route path="/activation/*" element={ <EmailConfigurationPage/> }/>
-                                        <Route path="/reset_password/*" element={ <ChangePasswordPage/> }/>
-                                        <Route path="/*" element={ <NotFoundPage/> }/>
-                                    </Routes>
-                                </SearchProvider>
-                            </ForgetModalProvider>
-                        </LoginModalProvider>
-                    </DakhelProvider>
-                </SigunUpProvider>
-            </div>
-        </CharityProvider>
-    );
-}
+                    )
+                }
 
 export default App
