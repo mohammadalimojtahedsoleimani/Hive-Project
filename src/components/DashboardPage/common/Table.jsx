7fa00cc888900da4b233177bcc88538771fb29df
amir-reza-programmer <amirrezazare130081@gmail.com>
2023-07-02 12:41:18 +0330

    feat: dashboard table implemented

--- src/components/DashboardPage/common/Table.jsx
+++ src/components/DashboardPage/common/Table.jsx
@@ -1,19 +1,52 @@
 import styles from "./Table.module.css";
-const Table = ({ titlesArr, dataArr }) => {
+import { useState } from "react";
+const Table = ({ titlesArr, dataArr, minHeight }) => {
   const itemBaseStyle = "text-[10px] xxl:text-[12px]";
   const itemStyle1 = itemBaseStyle + " text-[#4C4C4C]";
   const itemStyle2 = itemBaseStyle + " text-[#808080]";
+  const pageStyle = (page) => {
+    if (currentPage === page) {
+      return { color: "#9F9F9F" };
+    }
+    return { color: "#4C4C4C" };
+  };
+  const onPrevPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage((lastValue) => lastValue - 1);
+    }
+  };
+  const onNextPage = () => {
+    if (currentPage !== pageCount) {
+      setCurrentPage((lastValue) => lastValue + 1);
+    }
+  };
   const moneyToText = (money) => {
     return money.toLocaleString() + " تومان";
   };
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function getPageCount(totalItems, itemsPerPage) {
+    return Math.ceil(totalItems / itemsPerPage);
+  }
+
+  function getPageData(data, currentPage, itemsPerPage) {
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    return data.slice(start, end);
+  }
+  const itemsPerPage = 5;
+  const pageCount = getPageCount(dataArr.length, itemsPerPage);
+  const pageNumbers = [...Array(pageCount).keys()].map((num) => num + 1);
+  const pageData = getPageData(dataArr, currentPage, itemsPerPage);
   return (
     <div
+      style={{ minHeight: `${minHeight}%` }}
       className={
-        styles.table_shadow +
-        " inline-block rounded-lg overflow-hidden min-h-[22rem] xxl:min-h-[33rem]"
+        styles.table_shadow + ` inline-block rounded-lg overflow-hidden`
       }
     >
-      <table className={styles.table}>
+      <table style={{ minHeight: `${minHeight}%` }} className={styles.table}>
         <thead>
           <tr>
             {titlesArr.map((titleObj) => (
@@ -22,7 +55,7 @@ const Table = ({ titlesArr, dataArr }) => {
           </tr>
         </thead>
         <tbody>
-          {dataArr.map((data, i) => (
+          {pageData.map((data, i) => (
             <tr>
               <th>{data[titlesArr[0].value]}</th>
               <th>{data[titlesArr[1].value]}</th>
@@ -40,6 +73,40 @@ const Table = ({ titlesArr, dataArr }) => {
               </th>
             </tr>
           ))}
+          <div
+            className={
+              styles.pagination +
+              " py-[0.5rem] xxl:py-[1rem] gap-[0.4rem] xxl:gap-[0.6rem]"
+            }
+            dir="ltr"
+          >
+            <button
+              style={pageStyle(1)}
+              className="text-[10px] xxl:text-[12px]"
+              onClick={onPrevPage}
+            >
+              صفحه قبل
+            </button>
+            {pageNumbers.map((num) => (
+              <button
+                className={
+                  styles.pagination_num +
+                  " text-[10px] xxl:text-[12px] h-[1.5rem] xxl:h-[2.5rem] w-[1.5rem] xxl:w-[2.5rem]"
+                }
+                key={num}
+                onClick={() => setCurrentPage(num)}
+              >
+                {num}
+              </button>
+            ))}
+            <button
+              className="text-[10px] xxl:text-[12px]"
+              style={pageStyle(pageCount)}
+              onClick={onNextPage}
+            >
+              صفحه بعد
+            </button>
+          </div>
         </tbody>
       </table>
     </div>
