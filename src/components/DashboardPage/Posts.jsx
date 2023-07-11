6f171c1eeb32c91c521d9e1af193ab1ea19360e6
agha mamali <soleimanyali88@gmail.com>
2023-07-11 18:26:39 +0330

    posts changed

--- src/components/DashboardPage/Posts.jsx
+++ src/components/DashboardPage/Posts.jsx
@@ -14,6 +14,10 @@ import axios from "axios";
 import Table from "./common/Table";
 import moment from "moment";
 import { DakhelContext } from "../../context/DakhelContext";
+import { notify } from "../../helper/toast";
+import { useNavigate } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
 
 const nameFont = (value) => {
   if (value.length <= 15) {
@@ -103,6 +107,7 @@ const posts = [
 const Posts = () => {
   // variables
   const [newPost, setNewPost] = useState(false);
+  const navigate = useNavigate()
   const [fileData, setFileData] = useState(null);
   const pageSize = 5;
   const [currentPage, setCurrentPage] = useState(1);
@@ -111,7 +116,7 @@ const Posts = () => {
   const { isIn, setIsIn } = useContext(DakhelContext);
   let imagePath = "http://127.0.0.1:8000/media/ads/";
   const [alCharity, setAlCharity] = useState([]);
-
+  const delay = 2000;
   let value = "";
   const [data, setData] = useState({
     image: null,
@@ -185,7 +190,15 @@ const Posts = () => {
         },
       })
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
+        notify('ثبت پست با موفقیت انجام شد.'     , 'success')
+        const timer = setTimeout ( () => {
+          navigate(`/post/${response.data.id}`)
+
+
+
+
+        } , delay );
       })
       .catch((error) => {
         console.log("the error: ", error.response);
@@ -484,6 +497,7 @@ const Posts = () => {
           ارسال جهت بررسی
         </button>
       </div>
+      <ToastContainer/>
     </div>
   );
   return newPost ? newPostsContainer : oldPosts;
