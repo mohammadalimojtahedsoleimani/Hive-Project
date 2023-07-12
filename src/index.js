1f1180919d13c5b7b44ad57cd9bc4662ed1d4ef3
amir-reza-programmer <amirrezazare130081@gmail.com>
2023-07-13 00:01:03 +0330

    fix: delete unused bootstrap import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot ( document.getElementById ( "root" ) )
 root.render ( <BrowserRouter>
